e951d3a9ca2fca400640b21f5f365b7f2ed54a74
Atilla Yadik <atillayadik@gmail.com>
2021-07-31 17:58:15 -0500

    Api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>
+    Hello {{ value }}
+    <div>{{ message }}</div>
+  </div>`,
 })
 export class AppComponent {
   value = 'World';
+  message = '';
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get('/api/message')
+      .subscribe((res: any) => (this.message = res.text));
+  }
 }
